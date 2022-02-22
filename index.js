c336095156525c899912919025ffa04f233f6d5d
jodies383 <jodies383@gmail.com>
2022-02-22 16:30:09 +0200

    changed location reload

--- index.js
+++ index.js
@@ -5,31 +5,38 @@ let newGame = document.querySelector(".newGameMsg")
 let randomNumber = Math.ceil((Math.random() * 100))
 
 
-function newGameMessage(){
-    newGame.innerHTML = "New game started!"
-    document.getElementById("newGameMsg").style.color = "#b942f5";
+// function newGameMessage(){
+//     newGame.innerHTML = "New game started!"
+//     document.getElementById("newGameMsg").style.color = "#b942f5";
 
-    setTimeout(() => { newGame.innerHTML = "" }, 3000);
-}
+//     setTimeout(() => { newGame.innerHTML = "" }, 3000);
+// }
 
 submitGuessBtn.addEventListener('click', guessGame)
+
 function guessGame() {
     let userGuess = guess.value
 
     if (userGuess > randomNumber) {
         document.getElementById("message").style.color = "red";
         message.innerHTML = "Your guess is too high"
+        setTimeout(() => { message.innerHTML = "" }, 2000);
+
     }
     else if (userGuess < randomNumber) {
         document.getElementById("message").style.color = "#f57542";
         message.innerHTML = "Your guess is too low"
+        setTimeout(() => { message.innerHTML = "" }, 2000);
+
     }
     else if (userGuess == randomNumber) {
         message.innerHTML = `Correct, the secret number is ${userGuess}!`
         document.getElementById("message").style.color = "green";
-        setTimeout(() => { location.reload(); }, 2000)
+        setTimeout(() => { message.innerHTML = "New game started" }, 1000)
+        setTimeout(() => { message.innerHTML = "" }, 4000);
+        setTimeout(() => { location.reload(randomNumber); }, 5000)
+        
     }
 
-    setTimeout(() => { message.innerHTML = "" }, 2000);
 
 }
