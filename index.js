4344151edfcc90d07a8a6e54d413373cd06fff23
jodies383 <jodies383@gmail.com>
2022-02-22 14:38:10 +0200

    style additions

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ let randomNumber = Math.ceil((Math.random() * 100))
 
 function newGameMessage(){
     newGame.innerHTML = "New game started!"
+    document.getElementById("newGameMsg").style.color = "#b942f5";
+
     setTimeout(() => { newGame.innerHTML = "" }, 3000);
 }
 
@@ -15,13 +17,16 @@ function guessGame() {
     let userGuess = guess.value
 
     if (userGuess > randomNumber) {
+        document.getElementById("message").style.color = "red";
         message.innerHTML = "Your guess is too high"
     }
     else if (userGuess < randomNumber) {
+        document.getElementById("message").style.color = "#f57542";
         message.innerHTML = "Your guess is too low"
     }
     else if (userGuess == randomNumber) {
-        message.innerHTML = `Correct, the secret number is ${userGuess}`
+        message.innerHTML = `Correct, the secret number is ${userGuess}!`
+        document.getElementById("message").style.color = "green";
         setTimeout(() => { location.reload(); }, 2000)
     }
 
